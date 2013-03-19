1dbaaa1050487e52dd7ea671efb506b6088823be
Trea Hauet <treahauet@gmail.com>
2013-03-19 02:06:02 -0600

    Ummm.  Bring back the original home view though.

--- app/views/hello.php
+++ app/views/hello.php
@@ -1,1 +1,200 @@
-<h1>Hello World!</h1>
+<!doctype html>
+<html>
+	<head>
+		<title>Built with Laravel</title>
+		<link rel="stylesheet" href="assets/style/main.css">
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400' rel='stylesheet' type='text/css'>
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<div class="logo">
+				<img src="assets/images/laravel.png" alt="Built with Laravel">
+				<div class="bwl">
+					<small>built with</small>
+					laravel
+				</div>
+			</div>
+			<nav>
+				<ul>
+					<li class="active">
+						<a href="/">Popular</a>
+					</li>
+					<li>
+						<a href="/#new">Recently Added</a>
+					</li>
+					<li>
+						<a href="/grid">View All</a>
+					</li>
+					<li>
+						<a href="/#submit">Submit</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+		<div id="main">
+			<div class="mast">
+				<div class="slider">
+					
+				</div>
+				<div class="user">
+					<h3>Developer?</h3>
+					<p>Sign in or register with Github below to get started!</p>
+					<div id="signin">
+						<a class="btn github">
+							<i class="icon-github-sign"></i>
+							Sign in with Github
+						</a>
+<!-- 						<a class="btn google">
+							<i class="icon-google-plus-sign"></i>
+							Sign with Google
+						</a>
+						<a class="btn twitter">
+							<i class="icon-twitter-sign"></i>
+							Sign in with Twitter
+						</a> -->
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<h3>Tags</h3>
+					<ul class="nobull">
+						<li>
+							<a data-tag="laravel-4">
+								Blog
+							</a>
+						</li>
+						<li>
+							<a data-tag="laravel-3">
+								Corporate
+							</a>
+						</li>
+						<li>
+							<a data-tag="angularjs">
+								Commercial
+							</a>
+						</li>
+						<li>
+							<a data-tag="backbonejs">
+								eCommerce
+							</a>
+						</li>
+						<li>
+							<a href="">
+								Educational
+							</a>
+						</li>
+						<li>
+							<a data-tag="jquery">
+								Forum
+							</a>
+						</li>
+						<li>
+							<a href="">
+								Government
+							</a>
+						</li>
+						<li>
+							<a href="">
+								Media
+							</a>
+						</li>
+						<li>
+							<a href="">
+								News
+							</a>
+						</li>
+						<li>
+							<a href="">
+								Portal
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col">
+					<h2>Featured Laravel Projects</h2>
+					<ul class="nobull featured">
+						<li>
+							<img src="http://placehold.it/250x125">
+							<div class="info">
+								<h3>Project Title</h3>
+								<p>This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.</p>
+							</div>
+						</li>
+						<li>
+							<img src="http://placehold.it/250x125">
+							<div class="info">
+								<h3>Project Title</h3>
+								<p>This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.</p>
+							</div>
+						</li>
+						<li>
+							<img src="http://placehold.it/250x125">
+							<div class="info">
+								<h3>Project Title</h3>
+								<p>This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.</p>
+							</div>
+						</li>
+						<li>
+							<img src="http://placehold.it/250x125">
+							<div class="info">
+								<h3>Project Title</h3>
+								<p>This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.</p>
+							</div>
+						</li>
+						<li>
+							<img src="http://placehold.it/250x125">
+							<div class="info">
+								<h3>Project Title</h3>
+								<p>This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.  This is a short description about the project.</p>
+							</div>
+						</li>
+					</ul>
+					<div class="pages">
+						<ul class="pages">
+							<li>
+								<a href="#">
+									<i class="icon-angle-left"></i>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									1
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									2
+								</a>
+							</li>
+							<li class="active">
+								<!-- <a href="#"> -->
+									3
+								<!-- </a> -->
+							</li>
+							<li>
+								<a href="#">
+									4
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									5
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<i class="icon-angle-right"></i>	
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
