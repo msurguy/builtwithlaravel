0215a85cc364d9b44e1a45aa269896a63d6e2043
Maks Surguy <msurguy@gmail.com>
2013-03-21 12:15:21 -0700

    Ignoring sass cache

---
+++ app/views/gridstrap.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+		<title>Built with Laravel</title>
+
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="bootstrap/bootstrap-2.3.1.css">
+		<link rel="stylesheet" href="bootstrap/bootstrap-responsive-2.3.1.css">
+
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+		<script type="text/javascript" src="js/vendor/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js/vendor/bootstrap.min.js"></script>
+
+	</head>
+	<body>
+		<div class="navbar navbar-static-top">
+              <div class="navbar-inner">
+                <div class="container">
+                  <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </a>
+                  <img class="logo" src="assets/images/laravel.png" width="70" height="20" alt="Built with Laravel">
+                  <a class="brand" href="#">Built with Laravel</a>
+                  <div class="nav-collapse collapse navbar-responsive-collapse">  
+                    <ul class="nav pull-right">
+                      <li class='active'><a href="#">POPULAR</a></li>
+                      <li><a href="#">RECENTLY VIEWED</a></li>
+                      <li><a href="#">VIEW ALL</a></li>
+                      <li><a href="#">SUBMIT</a></li>
+                    </ul>
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+        </div>
+        <br>
+		<div class="container">
+			<div class="row">
+				<div class="span12">
+			      <div class="navbar navbar-inverse">
+			        <div class="navbar-inner">
+			          <div class="container">
+			            <ul class="nav nav-pills">
+				            <li class="disabled">
+				              <a href="#">ORDER BY:</a> 
+				            </li>
+			              <li class="active">
+			                <a href="#">NAME A-Z</a> 
+			              </li>
+			              <li>
+			                <a href="#">DATE ADDED</a> 
+			              </li>
+			              <li>
+			                <a href="#">RATING</a> 
+			              </li>
+			            </ul>
+			            <div class="pull-right">
+			            	<form class="navbar-search">
+			            	  <input type="text" class="search-query input-medium" placeholder="Search..."> 
+			            	</form>
+			            </div>
+			          </div>
+			        </div>
+			      </div>
+			  </div>
+			</div>
+			<div class="row">
+				<div class="span12">
+	            <ul class="thumbnails">
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	              <li class="span3">
+	                <div class="thumbnail">
+	                  	<img src="http://placehold.it/270x140">
+						<div class="caption">
+							<h4>Thumbnail label</h4>
+							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                  	<div class="stats">
+	                          <a href="#"><i class="icon-eye-open"></i></a> 521
+	                          <a href="#"><i class="icon-heart"></i></a> 114
+	                    	</div>
+						</div>
+	                </div>
+	              </li>
+	          	</ul>
+	          	</div>
+	      	</div>
+			<div class="row">
+				<div class="pagination pagination-centered">
+				  <ul>
+				    <li><a href="#">Prev</a></li>
+				    <li><a href="#">1</a></li>
+				    <li><a href="#">2</a></li>
+				    <li><a href="#">3</a></li>
+				    <li><a href="#">4</a></li>
+				    <li><a href="#">5</a></li>
+				    <li><a href="#">Next</a></li>
+				  </ul>
+				</div>
+			</div>
+		</div>
+	</body>
---
+++ app/views/home.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+		<title>Built with Laravel</title>
+
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="bootstrap/bootstrap-2.3.1.css">
+		<link rel="stylesheet" href="bootstrap/bootstrap-responsive-2.3.1.css">
+
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+		<script type="text/javascript" src="js/vendor/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js/vendor/bootstrap.min.js"></script>
+
+	</head>
+	<body>
+		<div class="navbar navbar-static-top">
+              <div class="navbar-inner">
+                <div class="container">
+                  <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </a>
+                  <img class="logo" src="assets/images/laravel.png" width="70" height="20" alt="Built with Laravel">
+                  <a class="brand" href="#">Built with Laravel</a>
+                  <div class="nav-collapse collapse navbar-responsive-collapse">  
+                    <ul class="nav pull-right">
+                      <li class='active'><a href="#">POPULAR</a></li>
+                      <li><a href="#">RECENTLY VIEWED</a></li>
+                      <li><a href="#">VIEW ALL</a></li>
+                      <li><a href="#">SUBMIT</a></li>
+                    </ul>
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+            </div>
+		<div class="container">
+			<div class="row">
+				<div class="span2 offset1">
+					<h3>Tags</h3>
+					<ul class="unstyled">
+						<li><a href="">Blog</a></li>
+						<li><a href="">Corporate</a></li>
+						<li><a href="">Commercial</a></li>
+						<li><a href="">eCommerce</a></li>
+					</ul>
+				</div>
+				<div class="span8">
+					<h3>Featured Laravel Projects</h3>
+					<hr>
+					<ul class="media-list">
+						<li class="media">
+							<a class="pull-left" href="#">
+								<img class="media-object" src="http://placehold.it/250x125">
+							</a>
+							<div class="media-body">
+								<p class="media-heading lead">Media heading</p>
+								<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+							</div>
+						</li>
+						<hr>
+						<li class="media">
+							<a class="pull-left" href="#">
+								<img class="media-object" src="http://placehold.it/250x125">
+							</a>
+							<div class="media-body">
+								<p class="media-heading lead">Media heading</p>
+								<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+							</div>
+						</li>
+						<hr>
+						<li class="media">
+							<a class="pull-left" href="#">
+								<img class="media-object" src="http://placehold.it/250x125">
+							</a>
+							<div class="media-body">
+								<p class="media-heading lead">Media heading</p>
+								<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+							</div>
+						</li>
+						<hr>
+						<li class="media">
+							<a class="pull-left" href="#">
+								<img class="media-object" src="http://placehold.it/250x125">
+							</a>
+							<div class="media-body">
+								<p class="media-heading lead">Media heading</p>
+								<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+							</div>
+						</li>
+						<hr>
+					</ul>
+
+					<div class="pagination pagination-centered">
+					  <ul>
+					    <li><a href="#">Prev</a></li>
+					    <li><a href="#">1</a></li>
+					    <li><a href="#">2</a></li>
+					    <li><a href="#">3</a></li>
+					    <li><a href="#">4</a></li>
+					    <li><a href="#">5</a></li>
+					    <li><a href="#">Next</a></li>
+					  </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
